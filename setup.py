507a5b77109f4d43ff7642f60fc97074a8f532c9
Fokingwone <adrianpedriza@hotmail.com>
2018-12-05 10:41:53 +0100

    Create setup.py
    :)

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prueba',
+     version='0.1',
+     description='The funniest joke in the world',
+     url='https://github.com/AdrianPedriza/Library_to_Install_from_code',
+     author='fokingwone',
+     author_email='adripedriza@gmail.com',
+     license='MIT',
+     packages=['src'],
+     zip_safe=False)
